d94dcecec419b6d06cfe3a329e4135a08e770d30
Shayne Stewart <sh.wc.smack@gmail.com>
2018-03-22 08:14:14 -0500

    Added Schedule View

--- main.py
+++ main.py
@@ -25,14 +25,23 @@ form["hprLogin$btnSubmit"] = "Login"
 file_mod_time = os.stat("output.html").st_mtime
 last_time = (time.time() - file_mod_time) / 60
 
+#last_time = 62
 if last_time > int(config['GENERAL']['CacheTime']):
     print("Getting Data")
     
     response = s.post(POST_PATH, data=form)
 
-    response_cache = open("output.html", "w")
+    #dump worksheet view
+    response_cache = open("worksheet.html", "w")
     response_cache.writelines(response.text)
     response_cache.close()
+
+    #dump schedule view
+    #payload = {'Action': config['PATHS']['ScheduleView']}
+    # print(config['PATHS']['WMTroot'] + "?Action=" + config['PATHS']['ScheduleView'])
+    response = s.get(config['PATHS']['WMTroot'] + "?Action=" + config['PATHS']['ScheduleView'])
+    sched_view = open("scheduleview.html", "w")
+    sched_view.writelines(response.text)
 else:
     print("Using cached version -- " + str(floor(last_time)) + " mins old.")
 
