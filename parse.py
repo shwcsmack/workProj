0e2a102c162d6d616738c394dca7896e8db63a81
Shayne Stewart <sh.wc.smack@gmail.com>
2018-03-22 22:16:26 -0500

    Added most of the code that finds the data

--- parse.py
+++ parse.py
@@ -17,7 +17,7 @@ def parseScheduleView():
     #find the container table that has the dropdowns
     dropdownsContainer = datatables[1]
     #find the container table that has the actual data table
-    dataContainer = datatables[2]
+    dataContainer = datatables[2].find("div").table
 
     #Get area and pay period
     selectedOptionsHTML = dropdownsContainer.find_all(selected = "selected")
@@ -25,7 +25,38 @@ def parseScheduleView():
     payperiod = selectedOptionsHTML[0].string
     print("Currently looking at: " + area + " and pay period: " + payperiod)
     
-    #Get the days
+    #Get the rows and loop
+    dataTableRows = dataContainer.find_all("tr")
+    for row in dataTableRows:
+        #find cells and loop
+        cells = row.find_all("td")
+        for cell in cells:
+            if cell.div:
+                if cell.div.div:
+                    #should be a cell with a shift in it inside an anchor tag
+                    if cell.div.div.a:
+                        #Shift [T0700]
+                        # print(cell.div.div.a.contents[4].strip())
+                        continue
+                    #this should be a leave cell
+                    else:
+                        #Leave [LV]
+                        # print(cell.div.div.string)
+                        continue
+                elif len(cell.div.contents) == 1:
+                    #TODO differentiate between names and count
+                    #Name [FF - Stewart]
+                    print(cell.div.string)
+                #else:
+                    #either a day or a help window link
+                    #print("Day or Help")
+            #else:
+                #blank cell
+                #print("blank cell")
+    
+    
+    
+    
     #loop through staff
 
     return dataout
