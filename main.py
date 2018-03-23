e2573de0bc3cda785b932c6e48441fa93bc0f7a6
Shayne Stewart <sh.wc.smack@gmail.com>
2018-03-22 20:54:01 -0500

    Added an error check for html file and got ride of IDE files

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ config.read('config.ini')
 #setup session object
 s = requests.session()
 
+#check if this system has run the code yet
+if os.path.isfile("worksheet.html"):
+    file_mod_time = os.stat("worksheet.html").st_mtime
+    last_time = (time.time() - file_mod_time) / 60
+else:
+    last_time = 9999999
 #check how old the current cache of files is
-file_mod_time = os.stat("worksheet.html").st_mtime
-last_time = (time.time() - file_mod_time) / 60
+
 
 #last_time = 450
 if last_time > int(config['GENERAL']['CacheTime']):
